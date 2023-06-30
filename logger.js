b2895a6f7bdad4a62d8e92639d390c8ed61e8c3c
Batuhan Bulut <ddurden5@gmail.com>
2023-06-30 15:03:14 +0300

    .envEXAMPLE added

--- logger.js
+++ logger.js
@@ -1,7 +1,15 @@
+//const fs = require('fs');
+
 const logTime = new Date().toISOString()
 function info(message) {
-    
-    console.log(`[INFO] | ${logTime} |`,message)
+    let logMessage = `[INFO] | ${logTime} |` + message
+    console.log(logMessage)
+//     fs.appendFile('./log.csv', logMessage + "\n", err => {
+//         if (err) {
+//           console.error(err);
+//         } 
+// })
+
 }
 
 module.exports = {
