defe88a3f5a708df2cbddd3806bde01b1b25d2bb
Batuhan Bulut <ddurden5@gmail.com>
2023-06-30 16:07:16 +0300

    docker container implemented closes #14

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = process.env.port
+const port = 3000
 
 const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require('./swagger_output.json')
