d0a883e2bc25be25ac8a82f485b9bf5751954914
Batuhan Bulut <ddurden5@gmail.com>
2023-07-01 16:15:36 +0300

    swagger UI updated

--- swagger.js
+++ swagger.js
@@ -1,6 +1,11 @@
 const swaggerAutogen = require('swagger-autogen')()
-
+const doc = {
+    info: {
+      version: '1.0.0',      // by default: '1.0.0'
+      title: 'Simple Http Server',        // by default: 'REST API'
+      description: '[ docker pull bulutbatuhan/simple-http-server ]<br>[ docker run -p 3000:3000 bulutbatuhan/simple-server ]<br>[ https://github.com/batuhan-bulut/simple-http-server ]',  // by default: ''
+    }}
 const outputFile = './swagger_output.json'
 const endpointsFiles = ['./index.js']
 
-swaggerAutogen(outputFile, endpointsFiles)
+swaggerAutogen(outputFile, endpointsFiles, doc)
