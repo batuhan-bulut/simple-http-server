f8149e0f4c63c49138c74cdd1ccbe0a7549bbf0b
Batuhan Bulut <ddurden5@gmail.com>
2023-07-01 17:08:49 +0300

    axios library deleted (urllib used)

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -1,59 +1,36 @@
-const axios = require('axios').default;
 const { request } = require('urllib');
 
 test('[GET] [Auth] Hidden Basic Auth', async () => {
-    const res = await axios
-        .get("http://localhost:3000/auth/hiddenbasicAuth/1/1", {
-            auth: {
-                username: 1,
-                password: 1
-            }
-        })
-    expect(res.data).toMatch(/Hidden Info, Auth Success/)
+    options = { auth: "1:1" }
+    const { data, res } = await request("http://localhost:3000/auth/hiddenbasicAuth/1/1", options)
+    expect(data.toString()).toMatch(/Hidden Info, Auth Success/)
 })
 
 
 test('[GET] [Uauthorized] Hidden Basic Auth', async () => {
-    const res = await axios
-        .get("http://localhost:3000/auth/hiddenbasicAuth/1/1")
-        .catch((error) => {
-            expect(error.response.data).toMatch(/Lorem Ipsum/)
-        })
+    const { data, _ } = await request("http://localhost:3000/auth/hiddenbasicAuth/1/1")
+    expect(data.toString()).toMatch(/Lorem Ipsum/)
 })
 
-
 test('[GET] [Auth] Basic Auth', async () => {
-    const res = await axios
-        .get("http://localhost:3000/auth/basicAuth/1/1", {
-            auth: {
-                username: 1,
-                password: 1
-            }
-        })
+    options = { auth: "1:1" }
+    const { _, res } = await request("http://localhost:3000/auth/basicAuth/1/1", options)
     const statusCode = res.status
     expect(statusCode).toEqual(200)
+
 })
 
 test('[GET] [Uauthorized] Basic Auth', async () => {
-    const res = await axios
-        .get("http://localhost:3000/auth/basicAuth/1/1", {
-            auth: {
-                username: 2,
-                password: 1
-            }
-        }).catch((error) => {
-            expect(error.response.status).toEqual(401)
-        });
-
+    options = { auth: "1:2" }
+    const { _, res } = await request("http://localhost:3000/auth/basicAuth/1/1", options)
+    const statusCode = res.status
+    expect(statusCode).toEqual(401)
 })
 
 test('[GET] [Auth] Digest Auth', async () => {
 
     const url = 'http://localhost:3000/auth/digestAuth/1/1';
-    const options = {
-        digestAuth: "1:1",
-    };
-
+    const options = { digestAuth: "1:1" };
     const { _, res } = await request(url, options)
     expect(res.status).toEqual(200)
 
@@ -62,10 +39,7 @@ test('[GET] [Auth] Digest Auth', async () => {
 test('[GET] [Uauthorized] Digest Auth', async () => {
 
     const url = 'http://localhost:3000/auth/digestAuth/1/1';
-    const options = {
-        digestAuth: "1:12",
-    };
-
+    const options = { digestAuth: "1:12" };
     const { _, res } = await request(url, options)
     expect(res.status).toEqual(401)
 
